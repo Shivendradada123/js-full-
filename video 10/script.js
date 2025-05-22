f11068b5bcd4f7ed6cfff987a2f8df39ca5dfac0
Shivedra123 <Shivendra@gmail.com>
2025-05-22 07:09:57 +0530

    good morining 7:09

--- video 10/script.js
+++ video 10/script.js
@@ -1,5 +1,12 @@
 //Arrays High Order Functions
 
+
+
+
+
+
+
+
 //  1. foreach use                                                                
 
 const student = ["shivendra", "aditya", "paisa", "chhot"];
@@ -8,9 +15,54 @@ const student = ["shivendra", "aditya", "paisa", "chhot"];
 
 student.forEach ((valu) => console.log(valu))
 
+// 2. .map use 
+
+
 const numbers = [1, 2, 3, 4, 5, 6, ];
 
-numbers.forEach( (num) => console.log(num * 2 ));
+function double(n) {
+    return n * 3
+}
+
+let newarry = numbers.map(double);
+console.log(newarry )
+
+//
+
+
+
+
+
+
+
+
+numbers.forEach( (num) => newarry.push(num * 2 ));
+console.log(newarry)
+
+// exapmle 01
+
+const phone = ["iqooz10", "oppo", "paisa", "chhot"];
+
+
+
+
+
+function print(s) {
+    console.log(s);
+}
+
+phone.forEach(print);
+
+//end of example 01
+///done 
+
+
+
+
+
+
+
+
 
 
 
