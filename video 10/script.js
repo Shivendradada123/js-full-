6faf24c19c769e868639d55808753d36c6c1fc3a
Shivedra123 <Shivendra@gmail.com>
2025-05-23 13:26:19 +0530

    yayamehuna

--- video 10/script.js
+++ video 10/script.js
@@ -54,7 +54,51 @@ function print(s) {
 phone.forEach(print);
 
 //end of example 01
-///done 
+
+
+// 3. .find use 
+
+const number = [1, 99, 3, 4, 8, 6];
+
+let ans = number.find((num) => num === 2*4);
+console.log(ans)
+
+
+//  04. .filters
+
+ const codders = [
+    {name: "shivendra", num: 84},
+    {name: "aditya", num: 90},
+    {name: "aura", num: 999}
+
+ ];
+ 
+ let pro = codders.filter(codders => codders.num > 50);
+
+ console.log(pro)
+
+
+ // 05. .slice use 
+
+ const numbersss = [1, 2, 3, 4, 5, 6, 7, 8,] 
+ let newarr = numbersss.slice(1, 6 );
+ console.log(newarr);
+
+
+ // 06 .Splice use
+
+ const numberss = [1, 2, 3, 4, 5, 6, 7, 8,] 
+ let newarrry = numberss.splice(1, 6 );
+ console.log(newarrry);
+ console.log(numberss)
+
+
+
+
+
+
+
+
 
 
 
