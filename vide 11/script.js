dec89f05c771a70151660d581b9b20b80345bd1b
Shivedra123 <Shivendra@gmail.com>
2025-05-24 14:15:27 +0530

    dfd

---
+++ vide 11/script.js
@@ -0,0 +1,33 @@
+
+//basic dom
+
+
+const value = prompt("whats your name?", "shivendra");
+console.log(value);
+
+
+
+
+
+// query selectors  -- first element par work hai
+// 1. tag name 
+
+ const body = document.querySelector("body");
+ console.log(body.innerHTML)
+
+
+ // 2. - id # 
+
+ let userid = document.querySelector("#userid");
+
+ userid.innerText = "mauhariya";
+
+
+ // 3. class .
+
+ const yaya = document.querySelector(".sk")
+ console.log(yaya.innerHTML)
+
+ 
+
+ 
