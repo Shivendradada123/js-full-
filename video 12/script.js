77422c4d4fdeb1448c35b847edbe5d5ed5b2c26d
Shivedra123 <Shivendra@gmail.com>
2025-05-25 16:18:38 +0530

    getelementbyid

---
+++ video 12/script.js
@@ -0,0 +1,25 @@
+// Select Element by id in javascript
+
+
+const ele = document.getElementById("userid")
+ele.innerText = "SHivendra mauhariya "
+ele.classList.add("green", "boob")
+
+
+
+console.log(ele.classList)
+
+
+// second exapmple
+
+const hub = document.getElementById("sk")
+
+hub.classList.add("mc")
+hub.style.backgroundColor = ("red")
+console.log(hub.parentElement)
+console.log(hub.id)
+
+
+
+
+
